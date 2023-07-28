b9843fc6c388eec7cf7078a7c49edc4a2ebe60c8
ZiqiXuu <zq952133@dal.ca>
2023-07-28 03:49:09 -0400

    version-4.0

--- src/component/SingleCallDetail.js
+++ src/component/SingleCallDetail.js
@@ -1,19 +1,12 @@
-import React, {useEffect} from 'react';
-import {useSelector} from "react-redux";
-import {clearLoading, setLoading} from "../store/actions/loadingAction";
-import {fetchMultipleActivities, fetchSingleActivities} from "../store/actions/activitiesAction";
-import IconButton from "@mui/material/IconButton";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import Avatar from "@mui/material/Avatar";
-import LinearProgress from "@mui/material/LinearProgress";
+import React from 'react';
 import './callDetailPage.css'
 
 
 const SingleCallDetail = (props) => {
     const call = props?.call
     console.log(call)
-    const [date, timeAndRest] = call?.created_at?.split("T")
-    const [hours, minutes] = timeAndRest.split(":")
+    const [date, timeAndRest = ""] = call?.created_at?.split("T") || []
+    const [hours = "", minutes = ""] = timeAndRest.split(":")
     const time = `${hours}:${minutes}`
 
     const secondsToHms = (seconds) => {
@@ -32,16 +25,16 @@ const SingleCallDetail = (props) => {
     }
 
     return (
-                        <div>
-                            <div className='content'>
-                                <div>From: {call?.from}</div>
-                                <div>To: {call?.to}</div>
-                                <div>Duration: {secondsToHms(call?.duration)}</div>
-                                <div>Call At: {date} {time}</div>
-                                <div>Type: {call?.call_type} call</div>
-                                <div>Aircall number: {call?.via}</div>
-                            </div>
-                        </div>
+        <div>
+            <div className='content'>
+                <div>From: {call?.from}</div>
+                <div>To: {call?.to}</div>
+                <div>Duration: {secondsToHms(call?.duration)}</div>
+                <div>Call At: {date} {time}</div>
+                <div>Type: {call?.call_type} call</div>
+                <div>Aircall number: {call?.via}</div>
+            </div>
+        </div>
 
     );
 };
